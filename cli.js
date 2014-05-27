f149d545544959b71e9209d5de83185fc8fc8ec1
Jessica Lord <jessica.lord@github.com>
2014-05-27 13:43:47 +0200

    more things

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var ghauth = require('ghauth')
+var minimist = require('minimist')
+var getIssues = require('index.js')
+
+var argv = minimist(process.argv.slice(2));
+
+var ghAuthOptions = {
+ // ~/.config/[configName].json will store the token
+configName : 'offline-issues',
+// (optional) whatever GitHub auth scopes you require
+scopes     : [ 'repo' ],
+// (optional) saved with the token on GitHub
+note       : 'This token is for the offline-issues module from NPM'
+}
+
+ghauth(ghAuthOptions, function(err, token) {
+  if (err) console.log(err)
+  getOptions(token)
+
+  // var token = { user: 'username',
+  // token: 'TOKEN' }
+})
---
+++ index.js
@@ -0,0 +1,9 @@
+var request = require('request')
+var mkdirp =  require('mkdirp')
+var GitHub = require('github-api')
+
+module.exports = function getIssues(options) {
+
+
+
+}
