e63f46289e098f50ac351150a218a5cca66f1a98
Jessica Lord <jlord@github.com>
2016-06-03 12:54:24 -0700

    Lint

--- src/writemarkdown.js
+++ src/writemarkdown.js
@@ -1,30 +1,30 @@
 var fs = require('fs')
+var path = require('path')
+
 var handlebars = require('handlebars')
-var mkdirp =  require('mkdirp')
+var mkdirp = require('mkdirp')
 
-module.exports = function writemarkdown(cb) {
-
+module.exports = function writemarkdown (cb) {
   mkdirp('md', function (err) {
-    if (err) return cb(err, "Error creating md directory.")
+    if (err) return cb(err, 'Error creating md directory.')
   })
 
   var issues = fs.readFileSync('comments.json')
   issues = JSON.parse(issues)
-  issues.forEach(function(issue) {
+  issues.forEach(function (issue) {
     var filename = repoDetails(issue.url)
-    var source = fs.readFileSync(__dirname + '/templates/markdown.hbs')
+    var source = fs.readFileSync(path.join(__dirname, '/templates/markdown.hbs'))
     var template = handlebars.compile(source.toString())
     var result = template(issue)
     fs.writeFile('md/' + filename + '.md', result, function (err) {
-      if (err) return cb(err, "Error writing md file.")
+      if (err) return cb(err, 'Error writing md file.')
     })
   })
   cb(null, 'Wrote markdown files.')
-
 }
 
-function repoDetails(issue) {
+function repoDetails (issue) {
   var a = issue.split('/')
-  var filename =  a[3] + '-' + a[4] + '-' + a[6]
+  var filename = a[3] + '-' + a[4] + '-' + a[6]
   return filename
 }
